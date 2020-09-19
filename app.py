2de6104796f04d2122fea49b0debe131ddfd5876
bSantos967 <bruno.bs.slb@gmail.com>
2020-09-20 00:40:38 +0100

    Portfolio page

--- app.py
+++ app.py
@@ -27,12 +27,12 @@ def submit_form():
         return 'something went wrong. Try again'
 
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data["email"]
-        subject = data["subject"]
-        message = data["message"]
-        file = database.write(f'\n{email},{subject},{message}')
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data["email"]
+#         subject = data["subject"]
+#         message = data["message"]
+#         file = database.write(f'\n{email},{subject},{message}')
 
 
 def write_to_csv(data):
