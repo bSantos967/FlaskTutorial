4de5a8cfaeb5fa55b2ea710974aa6ada41219c40
bSantos967 <bruno.bs.slb@gmail.com>
2020-09-20 00:35:11 +0100

    Portfolio page

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name=None):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['GET', 'POST'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'something went wrong. Try again'
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(data.values())
+
+
+if __name__ == '__main__':
+    app.run()
